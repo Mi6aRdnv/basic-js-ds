c8c8a499216fbd9b1e3a8820ce66cac077c4966b
Mi6aRdnv <kvberty@gmail.com>
2023-12-23 17:08:05 +0100

    feat: add binary-search-tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,47 +1,146 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+
 class BinarySearchTree {
+  constructor() {
+    this.rootProperty = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+      return this.rootProperty
+    }
+    
+
+  add(data) {
+    this.rootProperty = addNewNode(this.rootProperty, data);
+
+    function addNewNode(currentNode, data) {
+      if (!currentNode) {
+        return new Node(data);
+      }
+
+      if (currentNode.data === data) {
+        return currentNode;
+      }
+
+      if (data < currentNode.data) {
+        currentNode.left = addNewNode(currentNode.left, data);
+      } else {
+        currentNode.right = addNewNode(currentNode.right, data);
+      }
+
+      return currentNode;
+    }
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return searchWithin(this.rootProperty, data);
+
+    function searchWithin(node, data) {
+      if (!node) {
+        return false;
+      }
+
+      if (node.data == data) {
+        return true;
+      }
+
+      return data < node.data ? 
+        searchWithin(node.left, data) : 
+        searchWithin(node.right, data);
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+  find(data) {
+    return searchWithin(this.rootProperty, data);
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    function searchWithin(node, data) {
+      if (!node) {
+        return null;
+      }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+      if (node.data == data) {
+        return node;
+      }
+
+      return data < node.data ? 
+        searchWithin(node.left, data) : 
+        searchWithin(node.right, data);
+    }
+  }
+    // returns node with the  data  if node with the data exists in the tree and  null  otherwise
+
+  remove(data) {
+
+    this.rootProperty = removeNode(this.rootProperty, data);
+
+    function removeNode(node, data) {
+
+      if (data < node.data) {
+        node.left = removeNode(node.left, data);
+        return node;
+      } else if (node.data < data) {
+        node.right = removeNode(node.right, data);
+        return node;
+      } else {
+        if (!node.left && !node.right) {
+          return null;
+        }
+
+        if (!node.left) {
+          node = node.right;
+          return node;
+        }
+
+        if (!node.right) {
+          node = node.left;
+          return node;
+        }
+
+        let minFromRight = node.right;
+        while (minFromRight.left) {
+          minFromRight = minFromRight.left;
+        }
+        node.data = minFromRight.data;
+
+        node.right = removeNode(node.right, minFromRight.data);
+
+        return node;
+      }
+    }
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.rootProperty) {
+      return;
+    }
+
+    let node = this.rootProperty;
+    while (node.left) {
+      node = node.left;
+    }
+
+    return node.data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.rootProperty) {
+    return;
   }
+
+  let node = this.rootProperty;
+  while (node.right) {
+    node = node.right;
+  }
+
+  return node.data;
+}
 }
 
 module.exports = {
